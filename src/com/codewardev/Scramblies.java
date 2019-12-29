5cc9fc62db46d283d25e2a3ffa8f4fb487a9a540
Dale Wang <dalinwanglhtz@gmail.com>
2019-12-29 20:21:01 +1100

    started

---
+++ src/com/codewardev/Scramblies.java
@@ -0,0 +1,12 @@
+package com.codewardev;
+
+// https://www.codewars.com/kata/scramblies/train/java
+
+public class Scramblies {
+
+	public static boolean scramble(String str1, String str2) {
+		
+		return false;
+	}
+
+}
