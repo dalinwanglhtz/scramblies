628182b9cc1aa0c0bde4356d027058fb0dd08e8d
Dale Wang <dalinwanglhtz@gmail.com>
2019-12-31 21:47:53 +1100

    refined version

--- src/com/codewardev/Scramblies.java
+++ src/com/codewardev/Scramblies.java
@@ -6,28 +6,9 @@ import java.util.Arrays;
 
 public class Scramblies {
 
-	public static boolean scramble(String str1, String str2) {
-		int[] str1Array = new int[Arrays.stream(str1.split("")).mapToInt(x->x.toCharArray()[0]).max().getAsInt()+1];
-		Arrays.fill(str1Array, 0);
-		System.out.println("size of str1Array: "+str1Array.length);
-		
-		char[] str1ArrayContent = str1.toCharArray();
-		for(int i=0; i<str1ArrayContent.length; i++) {
-			str1Array[str1ArrayContent[i]]++;
-		}
-		
-		printArray(str1Array);
-		
-		int[] str2Array = new int[Arrays.stream(str2.split("")).mapToInt(x->x.toCharArray()[0]).max().getAsInt()+1];
-		Arrays.fill(str2Array, 0);
-		System.out.println("size of str2Array: "+str2Array.length);
-		
-		char[] str2ArrayContent = str2.toCharArray();
-		for(int i=0; i<str2ArrayContent.length; i++) {
-			str2Array[str2ArrayContent[i]]++;
-		}
-		
-		printArray(str2Array);
+	public static boolean scramble(String str1, String str2) {	
+		int[] str1Array = getStrArray(str1);
+		int[] str2Array = getStrArray(str2);
 		
 		for(int i=0; i<str2Array.length; i++) {
 			if(str2Array[i] > str1Array[i]) {
@@ -38,13 +19,20 @@ public class Scramblies {
 		return true;
 	}
 
-	private static void printArray(int[] str1Array) {
-		for(int i=0; i<str1Array.length; i++) {
-			if(str1Array[i] != 0) {
-				System.out.println("char: "+(char) i+" occuring "+str1Array[i]);
-			}
-		}
+	private static int[] getStrArray(String str) {
+		int[] array = new int[Arrays.stream(str.split("")).mapToInt(x->x.toCharArray()[0]).max().getAsInt()+1];
+		Arrays.fill(array, 0);
 		
+		fillStrArray(array, str);
+		
+		return array;
+	}
+
+	private static void fillStrArray(int[] array, String str) {
+		char[] strArrayContent = str.toCharArray();
+		for(int i=0; i<strArrayContent.length; i++) {
+			array[strArrayContent[i]]++;
+		}
 	}
 
 }
